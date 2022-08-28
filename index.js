673757056da2da144909939fade1ded27606d12a
niskPatel1 <ramoliyanikhil252525@gmail.com>
2022-08-28 09:26:08 +0530

    done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(Express.json());
 app.get("/", (req,res)=>{
     res.send("hello world")
 })
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
     console.log("server is running at port : " + port );
